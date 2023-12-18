6ae8dc4a0ef5ab5172325aefdcab17dd066fdc2f
Madhu-9121 <madhusudhanc135@gmail.com>
2023-12-18 15:34:51 +0530

    package file edited

--- routes/lama.routes.js
+++ routes/lama.routes.js
@@ -5,7 +5,7 @@ const { route } = require("../app");
 
 router.get("/user",getUsers);
 router.get('/:email',getUserByPerson)
-router.post('/user', addUser);
+router.post('/user', addUser);  
 router.post("/user/:email/projects",addNewProject)
 router.post('/user/:email/newtask',addNewTask)
 router.patch('/user/:email',updateContent)
